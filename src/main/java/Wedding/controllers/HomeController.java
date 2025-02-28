449eabf929344b692c90f1ccc047b2d0c281f02f
sourourgharbi <sourour.gharbi@esprit.tn>
2025-02-28 20:52:41 +0100

    resheb+voiture

--- src/main/java/Wedding/controllers/HomeController.java
+++ src/main/java/Wedding/controllers/HomeController.java
@@ -2,9 +2,13 @@ package Wedding.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Side;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -59,24 +63,48 @@ public class HomeController {
 
 
     @FXML
-    private void goToDriveAndStay() {
+    private void goToDriveAndStay(MouseEvent event) {
+        ContextMenu contextMenu = new ContextMenu();
+
+        MenuItem voitureItem = new MenuItem("Voiture");
+        MenuItem hebergementItem = new MenuItem("Hébergement");
+
+        voitureItem.setOnAction(e -> goToVoiture());
+        hebergementItem.setOnAction(e -> goToHebergement());
+
+        contextMenu.getItems().addAll(voitureItem, hebergementItem);
+
+        // Afficher le menu à la position du bouton
+        Node source = (Node) event.getSource();
+        contextMenu.show(source, Side.BOTTOM, 0, 0);
+    }
+    private void goToVoiture() {
         try {
-            // Charger le fichier FXML de la page des produits
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/hebergementclient.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/voitureclient.fxml"));
             Parent root = loader.load();
-
-            // Récupérer la scène actuelle
             Stage stage = (Stage) loginButton.getScene().getWindow();
-
-            // Changer la scène pour afficher la page des produits
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
+            stage.setScene(new Scene(root));
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Erreur lors du chargement de la page des produits.");
+            System.out.println("Erreur lors du chargement de la page des voitures.");
         }
     }
+
+    private void goToHebergement() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/hebergementclient.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Erreur lors du chargement de la page des hébergements.");
+        }
+    }
+
+
 
     @FXML
     private void goToInvite() {
